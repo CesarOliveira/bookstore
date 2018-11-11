c1b8cd162a79a210fbacd2acfb2f83218a492194
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2018-11-11 20:41:47 -0200

    Implementing update number of likes in Category after like the book

--- app/helpers/like_helper.rb
+++ app/helpers/like_helper.rb
@@ -1,5 +1,6 @@
 module LikeHelper
 	def like_book(book)
 		book.increase_like
+		book.category.increase_like
 	end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+	def increase_like
+  	update_attributes(likes: self.likes + 1)
+  end
 end
