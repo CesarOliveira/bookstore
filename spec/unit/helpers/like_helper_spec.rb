8cd815e53c51f078c464728b5ce33a3bcde39e4c
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2018-11-12 07:54:36 -0200

    Add test for helper LikeHelper

---
+++ spec/unit/helpers/like_helper_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe LikeHelper, type: :helper do
+  describe '#like_book' do
+      
+    let(:book_likes) { Faker::Number.number(3).to_i }
+    let(:category_likes) { Faker::Number.number(3).to_i }
+    let!(:category) { create(:category, likes: category_likes) }
+    let!(:book) { create(:book, likes: book_likes, category: category) }
+
+      subject do
+        helper.like_book(book)
+        book
+      end
+
+      it 'should increase the amount of likes in books' do
+        expect(subject.likes).to eq book_likes + 1
+      end
+
+      it 'should increase the amount of likes in category' do
+        expect(subject.category.likes).to eq category_likes + 1
+      end
+  end
+end
