bbbc4f6b29752b3416340dc88e1d45efeace7e1b
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2018-11-11 23:27:22 -0200

    Improve test on book model

--- spec/unit/models/book_spec.rb
+++ spec/unit/models/book_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe Book, type: :model do
       is_expected.to have_db_column(:description).of_type(:text)
       is_expected.to have_db_column(:likes).of_type(:integer)
       is_expected.to have_db_index(:category_id)
+      is_expected.to have_db_column(:category_id).of_type(:integer)
+    end
+  end
+
+describe 'relationships' do
+    it do
+      is_expected.to belong_to(:category)
     end
   end
 end
