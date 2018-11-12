9ce7d9441936dc96f57d25099f84618bd41be269
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2018-11-12 08:26:08 -0200

    Create test for show route in Category

--- spec/unit/controllers/categories_controller_spec.rb
+++ spec/unit/controllers/categories_controller_spec.rb
@@ -22,4 +22,47 @@ RSpec.describe CategoriesController, type: :controller do
       is_expected.to match_response_schema('categories/index')
     end
   end
+
+  describe 'GET #show' do
+    subject do
+      response
+    end
+
+    context 'with existent category' do
+      let!(:category) { create(:category) }
+      let(:category_params) do 
+        { id: category.id } 
+      end
+
+      before do
+        get :show, params: category_params
+      end
+
+      it 'returns status ok' do
+        is_expected.to have_http_status(:ok)
+      end
+
+      it 'matches the json schema' do
+        is_expected.to match_response_schema('categories/show')
+      end
+    end
+
+    context 'with inexistent category' do
+      let(:category_params) do
+        { id: Faker::Number.number(3).to_i }
+      end
+
+      before do
+        get :show, params: category_params
+      end
+
+      subject do
+        response
+      end
+
+      it 'returns status not_found' do
+          is_expected.to have_http_status(:not_found)
+      end
+    end
+  end
 end
